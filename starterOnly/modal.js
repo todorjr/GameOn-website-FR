6afdea77d9e7a1532987e49c959be7c1f5816785
lazar <eleazartodor@gmail.com>
2022-06-15 15:17:03 +0200

    Change function valider()

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -21,6 +21,8 @@ function launchModal() {
 }
 
 //creating variables with all field inputs
+const form = document.querySelector("#formId");
+
 let userName = document.querySelector("#first");
 let userSurname = document.querySelector("#last");
 let userEmail = document.querySelector("#email");
@@ -28,8 +30,8 @@ let userBirthDay = document.querySelector("#birthdate");
 let userLocation = document.getElementsByName("location"); //verify if this is correct to access to all locations
 let btnSubmit = document.querySelector(".btn-submit");
 
-let validEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-
+const regexEmail =
+  /^[a-z0-9!#$ %& '*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&' * +/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/g;
 // creating submiting function validate()
 
 function valider() {
@@ -37,14 +39,18 @@ function valider() {
   let prenom = userSurname.value;
   let date = userBirthDay.value;
   let email = userEmail.value;
+  // debugger;
   if (
-    nom == "" &&
-    prenom == "" &&
-    date == "" &&
-    email == "" &&
-    validEmail.test(userEmail)
+    nom !== null &&
+    prenom !== null &&
+    date !== null &&
+    email !== null &&
+    regexEmail.test(email)
   ) {
     return alert("blabalaa");
+  } else {
+    return alert("not working");
   }
 }
+
 btnSubmit.addEventListener("click", valider());
