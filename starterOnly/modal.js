40dc621b09cba31bfc685a33e079184c6b8cf1ab
lazar <eleazartodor@gmail.com>
2022-06-15 14:26:46 +0200

    First commit

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -20,4 +20,31 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+//creating variables with all field inputs
+let userName = document.querySelector("#first");
+let userSurname = document.querySelector("#last");
+let userEmail = document.querySelector("#email");
+let userBirthDay = document.querySelector("#birthdate");
+let userLocation = document.getElementsByName("location"); //verify if this is correct to access to all locations
+let btnSubmit = document.querySelector(".btn-submit");
 
+let validEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+// creating submiting function validate()
+
+function valider() {
+  let nom = userName.value;
+  let prenom = userSurname.value;
+  let date = userBirthDay.value;
+  let email = userEmail.value;
+  if (
+    nom == "" &&
+    prenom == "" &&
+    date == "" &&
+    email == "" &&
+    validEmail.test(userEmail)
+  ) {
+    return alert("blabalaa");
+  }
+}
+btnSubmit.addEventListener("click", valider());
