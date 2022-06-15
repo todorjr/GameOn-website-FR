42caa511b83a596ff05563bac6b875f343a2d8f1
lazar <eleazartodor@gmail.com>
2022-06-15 16:54:48 +0200

    DOM Elements

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,13 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+let userName = document.querySelector("#first");
+let userSurname = document.querySelector("#last");
+let userEmail = document.querySelector("#email");
+let userBirthDay = document.querySelector("#birthdate");
+let userQuantity = document.querySelector("#quantity");
+let userLocations = document.getElementsByName("location"); //verify if this is correct to access to all locations
+let btnSubmit = document.querySelector(".btn-submit");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,23 +27,14 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-//creating variables with all field inputs
-// const form = document.querySelector("#formId");
-
-let userName = document.querySelector("#first");
-let userSurname = document.querySelector("#last");
-let userEmail = document.querySelector("#email");
-let userBirthDay = document.querySelector("#birthdate");
-let userQuantity = document.querySelector("#quantity");
-let userLocations = document.getElementsByName("location"); //verify if this is correct to access to all locations
-let btnSubmit = document.querySelector(".btn-submit");
+// RegEx Declarations
 const regexEmail =
   /^[a-z0-9!#$ %& '*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&' * +/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/g;
 const regexName = /^[A-Za-zÀ-ÿ-]{2,}$/i;
 const regexBirthDay = /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/;
 const regexQuantity = /^(0|[1-9][0-9]*)$/;
 
-// creating validLocation ()
+// Creating validLocation ()
 function validLocation() {
   let radioBtnChecked = false;
   userLocations.forEach((location) => {
@@ -46,11 +44,10 @@ function validLocation() {
   });
   return radioBtnChecked;
 }
-//creating modal for sending message after form is validated
 
 // create error messages ,change html by adding <p>ERROR MESSAGES</p> under input field
 
-// creating submiting function validate()
+// Creating submiting function validate()
 function valider() {
   debugger;
   let nom = userName.value;
