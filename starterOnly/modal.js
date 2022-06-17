c2f7923843b56fcccf92bde79143ad21d46e2143
lazar <eleazartodor@gmail.com>
2022-06-17 12:32:54 +0200

    Refacturing formData nad submit functions

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -27,68 +27,50 @@ let formData = document.querySelectorAll(".formData");
  * Generate a error element.
  * @param {string} message Text to display
  */
-function createError(message) {
+
+// function createError will create error if field is not properly inserted or is empty
+function createError(id, message) {
+  removeError(id);
+  const elt = document.getElementById(id);
+  elt.style.border = "5px solid red";
   let p = document.createElement("p");
   p.classList.add("error");
   p.textContent = message;
   p.style.color = "red";
-  p.style.fontFamily = "Impact,Charcoal,sans-serif";
-  return p;
+  elt.parentElement.appendChild(p);
 }
-
-let p = document.createElement("p");
-p.classList.add("error");
-p.textContent = "dffdsf";
-formData.forEach((data) => data.appendChild(createError("Error")));
+// function removeError will delete all errors after creation if field is properly inserted
+function removeError(id) {
+  const ele = document.getElementById(id);
+  const errors = Array.from(ele.parentElement.getElementsByClassName("error"));
+  console.log("errors", errors);
+  errors.forEach((err) => ele.parentElement.removeChild(err));
+}
 console.log(formData);
 
-//creating confirmation message
-
-const textValidation = document.createElement("h3");
-textValidation.style.textAlign = "center";
-textValidation.innerHTML = "Merci,Votre réservation a bien été enregistrée";
-
-//*test validation functions
-function isValidName() {
-  const userName = document.querySelector("#first");
-  const userSurname = document.querySelector("#last");
-  const regexName = /^[A-Za-zÀ-ÿ-]{2,}$/i;
-  if (
-    userName !== null &&
-    userSurname !== null &&
-    regexName.test(userName.value, userSurname.value)
-  ) {
-    return true;
-  } else {
-    return false;
-  }
+// function validationMessage will send validation message to the user
+function validationMessage() {
+  const textValidation = document.createElement("h3");
+  textValidation.style.textAlign = "center";
+  textValidation.innerHTML = "Merci,Votre réservation a bien été enregistrée";
 }
-function isValidEmail() {
+
+// validation functions for all fields
+function isValidName(value = "") {
+  const regexName = /^[A-Za-zÀ-ÿ-]{2,}$/i;
+  return value !== "" && regexName.test(value);
+}
+function isValidEmail(value = "") {
   const regexEmail =
     /^[a-z0-9!#$ %& '*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&' * +/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/g;
-  const userEmail = document.querySelector("#email");
-  if (userEmail !== null && regexEmail.test(userEmail.value)) {
-    return true;
-  } else {
-    return false;
-  }
+  return value !== "" && regexEmail.test(value);
 }
 // Creating isValidBirthday function
-function isValidBirthday() {
+function isValidBirthday(value = "") {
   const regexBirthDay = /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/;
-  const userBirthDay = document.querySelector("#birthdate");
-  if (userBirthDay !== null && regexBirthDay.test(userBirthDay.value)) {
-  }
+  return value !== "" && regexBirthDay.test(value);
 }
 
-// Creating a function isValidQuantity
-function isValidQuantity() {
-  const regexQuantity = /^(0|[1-9][0-9]*)$/;
-  const userQuantity = document.querySelector("#quantity");
-  if (userQuantity !== null && regexQuantity.test(userQuantity.value)) {
-  }
-}
-// Creating validLocation ()
 function isValidLocation() {
   let radioBtnChecked = false;
   userLocations.forEach((location) => {
@@ -98,43 +80,58 @@ function isValidLocation() {
   });
   return radioBtnChecked;
 }
-// Creating isChecked ()  function
+// validators is object where we will stored our key and values from formData (user input) (key is name of fields from form and value is user input)
+const validators = {
+  first: {
+    validationFunction: isValidName,
+    message: "Le prénom n'est pas valide",
+  },
+  last: { validationFunction: isValidName, message: "Le nom n'est pas valide" },
+  email: {
+    validationFunction: isValidEmail,
+    message: "L'email' n'est pas valide",
+  },
+  birthdate: {
+    validationFunction: isValidBirthday,
+    message: "La date de naissance n'est pas valide",
+  },
+};
 
-function isChecked() {}
-
-// Creating submiting function validate()
-function valider() {
-  // debugger;
-}
-
-formElement.addEventListener("submit", function (event) {
+function formSubmit(event) {
   // prevent native form to send
   event.preventDefault();
 
-  // get form data from #formId element
-  const formData = new FormData(this);
+  // get form data
+  const formData = Object.fromEntries(new FormData(this));
+  const formDataEntries = Object.entries(formData);
+  const errors = [];
+  const valid = [];
 
-  // get formData as an object with key=value
-  const { first, last, email } = Object.fromEntries(formData);
-  console.log(Object.fromEntries(formData));
-
-  // initialiser le compteur d'erreur à 0
-  //create for every field test functions
-
-  //! if conditional will check if fields are 0 or 1 and depends on that will return message of succes or error message
-  if (!isValidName(first, last)) {
-    // createError sur le champ "#first"
-    // compter une nouvelle erreur
-  } else {
-    console.log("l'nom n'est pas  valide");
+  for (const entry of formDataEntries) {
+    const keyName = entry[0];
+    const value = entry[1];
+    if (validators[keyName]) {
+      const validationFn = validators[keyName].validationFunction;
+      if (validationFn(value)) {
+        valid.push(keyName);
+      } else {
+        const errorMessage = validators[keyName].message;
+        errors.push({ id: keyName, message: errorMessage });
+      }
+    }
   }
 
-  if (!isValidEmail(email)) {
-    console.log("l'email est  valide");
-  } else {
-    console.log("l'email n'est pas valide");
+  console.log(errors);
+
+  errors.forEach(({ id, message }) => createError(id, message));
+  valid.forEach((id) => removeError(id));
+
+  if (errors.length === 0) {
+    // submit validation
   }
 
   // si le compteur est à 0 alors j'affiche le message de succès
   // sinon j'affiche le message d'erreur
-});
+}
+
+formElement.addEventListener("submit", formSubmit);
